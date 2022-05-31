ab171ca3954a84af83ba8debb3c3df6b60102709
孔新恩 <1547121779@qq.com>
2022-05-31 22:25:33 +0800

    按需引入vant组件

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入公共样式
 import './styles/index.less'
+// 按需引入vant
+import './config/vant.config.js'
 
+// 关闭生产提示
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
